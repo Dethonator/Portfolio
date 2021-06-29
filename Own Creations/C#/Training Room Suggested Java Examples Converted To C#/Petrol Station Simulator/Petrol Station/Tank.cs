776762610c0f191ad7702e833e5e9231878b1c22
Henry Brooks <henry_brooks_developer@outlook.com>
2021-06-29 15:50:47 +0100

    Petrol station simulator complete

---
+++ Own Creations/C#/Training Room Suggested Java Examples Converted To C#/Petrol Station Simulator/Petrol Station/Tank.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrol_Station
+{
+    class Tank
+    {
+        private float _level;
+
+        public float Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+        private float _capacity;
+        public float Capacity
+        {
+            get { return _capacity;}
+            set { _capacity = value; }
+        }
+
+        public Tank(float level, float capacity)
+        {
+            _level = level;
+            _capacity = capacity;
+        }
+    }
+}
